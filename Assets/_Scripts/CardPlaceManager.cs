5e50568023e15d80bef3bb85706fc3d1271b60c2
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-18 18:09:30 +0300

    place manager

---
+++ Assets/_Scripts/CardPlaceManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+    public class CardPlaceManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/TouchManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class TouchManager : MonoBehaviour
+    {
+        private void OnMouseUp()
+        {
+            Debug.Log("On Mouse Up!");
+        }
+
+
+        private void OnMouseDrag()
+        { 
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10); 
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition); 
+            gameObject.transform.position = objPosition;
+        }
+    }
+}
