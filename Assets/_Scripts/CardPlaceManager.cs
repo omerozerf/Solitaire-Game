2c50f424a16b47a5194a17cd34b625331e48aa8b
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-18 18:20:49 +0300

    Update CardPlaceManager.cs

--- Assets/_Scripts/CardPlaceManager.cs
+++ Assets/_Scripts/CardPlaceManager.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace _Scripts
 {
     public class CardPlaceManager : MonoBehaviour
     {
+        public List<GameObject> cardPlaceList;
         
     }
 }
