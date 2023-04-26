1739d57d9f58fa7a04bf09304c9b6d6d89e291f1
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-26 10:29:52 +0300

    card slot

---
+++ Assets/_Scripts/CardSlot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class CardSlot : MonoBehaviour
+    {
+        [SerializeField] private Vector2 size = Vector2.one;
+
+        private void OnValidate()
+        {
+            transform.localScale = size;
+
+
+            Debug.Log("On Validate!");
+        }
+    }
+}
---
+++ Assets/_Scripts/CardSlotManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Scripts
+{
+    public class CardSlotManager : MonoBehaviour
+    { 
+        public List<CardSlot> cardSlotList;
+    }
+}
