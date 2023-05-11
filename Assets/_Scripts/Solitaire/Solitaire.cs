f0b67cce7213a65a5838335e104d8681c2be7697
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-05-11 10:38:01 +0300

    deal

--- Assets/_Scripts/Solitaire/Solitaire.cs
+++ Assets/_Scripts/Solitaire/Solitaire.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class Solitaire : MonoBehaviour
 {
     public static string[] suitArray = new string[]
-        { "CLUBS", "DIAMONDS", "HEARTS", "SPADES" };
+        { "Clubs", "Diamonds", "Hearts", "Spades" };
     public static string[] valueArray = new string[]
         { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
     public Sprite[] cardFaceArray;
     public List<string> deckList;
-
+    public GameObject cardPrefab;
 
     private void Start()
     {
@@ -24,6 +24,7 @@ public class Solitaire : MonoBehaviour
     {
         deckList = GenerateDeck();
         Shuffle(deckList);
+        SolitaireDeal();
 
         foreach (string card in deckList)
         {
@@ -40,7 +41,7 @@ public class Solitaire : MonoBehaviour
         {
             foreach (string value in valueArray)
             {
-                newDeckList.Add(suit + "-" + value);
+                newDeckList.Add(suit + " - " + value);
             }
         }
 
@@ -60,6 +61,28 @@ public class Solitaire : MonoBehaviour
             T temp = list[k];
             list[k] = list[n];
             list[n] = temp;
+        }
+    }
+
+
+    void SolitaireDeal()
+    {
+        float yOffset = 0f;
+        float zOffset = 0.03f;
+        
+        
+        foreach (string card in deckList)
+        {
+            Vector3 rotation = new Vector3(0, 0, 270);
+            Vector3 position = transform.position;
+            
+            GameObject newCard = Instantiate
+                (cardPrefab, new Vector3(position.x, position.y - yOffset, position.z - zOffset), Quaternion.Euler(rotation));
+
+            yOffset = yOffset + 0.1f;
+            zOffset = zOffset + 0.03f;
+            
+            newCard.name = card;
         }
     }
 }
