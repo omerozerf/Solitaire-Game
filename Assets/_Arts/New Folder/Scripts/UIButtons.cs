d8af7b5b67a83416ced0c57714e126f4b1423f18
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-05-13 15:05:21 +0300

    a

---
+++ Assets/_Arts/New Folder/Scripts/UIButtons.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    public GameObject highScorePanel;
+
+    private UserInput userInput;
+    private Solitaire solitaire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        solitaire = FindObjectOfType<Solitaire>();
+        userInput = FindObjectOfType<UserInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAgain()
+    {
+        highScorePanel.SetActive(false);
+        ResetScene();
+    }
+
+    public void ResetScene()
+    {
+        userInput.move = 0;
+        userInput.score = 0;
+        solitaire.timer = 0;
+        // find all the cards and remove them
+        UpdateSprite[] cards = FindObjectsOfType<UpdateSprite>();
+        foreach (UpdateSprite card in cards)
+        {
+            Destroy(card.gameObject);
+        }
+        ClearTopValues();
+        // deal new cards
+        FindObjectOfType<Solitaire>().PlayCards();
+    }
+
+    void ClearTopValues()
+    {
+        Selectable[] selectables = FindObjectsOfType<Selectable>();
+        foreach (Selectable selectable in selectables)
+        {
+            if (selectable.CompareTag("Top"))
+            {
+                selectable.suit = null;
+                selectable.value = 0;
+            }
+        }
+    }
+
+}
