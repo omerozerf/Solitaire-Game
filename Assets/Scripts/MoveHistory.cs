e3ff3a82edb3eb1ee35904e1e290e03ed2159b82
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-05-16 23:57:33 +0300

    change file

---
+++ Assets/Scripts/MoveHistory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Solitaire
+{
+    public class MoveHistory : MonoBehaviour
+    {
+        private Stack<UndoData> undoDataStack = new Stack<UndoData>();
+
+
+        public void UndoMove()
+        {
+            if (undoDataStack.Count > 0)
+            {
+                UndoData undoData = undoDataStack.Pop();
+                
+            }
+        }
+    }
+}
