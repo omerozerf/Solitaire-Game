47eb7f5f9c0a81a7167662e5572362dae3ba0bc2
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-05-20 19:04:18 +0300

    card drag

---
+++ Assets/Scripts/CardDrag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _Scripts;
+using UnityEngine;
+
+public class CardDrag : MonoBehaviour
+{
+    private MoveToPosition moveToPosition;
+
+
+    private void Awake()
+    {
+        moveToPosition = GetComponent<MoveToPosition>();
+    }
+    
+    /*
+    private void OnMouseDrag()
+    { 
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10); 
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        moveToPosition.SetTargetPos(objPosition);
+    }
+     */
+}
